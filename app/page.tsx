aa879a583f4df7c9dc00b47230ea430a0f45465b
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-03-10 16:28:09 +0330

    add loading

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 
 export default async function Home() {
-  // const fff = async () => {
-  //   return new Promise((resolve, reject) => {
-  //     setTimeout(() => resolve(true), 2000);
-  //   });
-  // };
-  // await fff();
+  const fff = async () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(true), 2000);
+    });
+  };
+  await fff();
   return (
     <div className="App">
       <h1>antd version: </h1>
