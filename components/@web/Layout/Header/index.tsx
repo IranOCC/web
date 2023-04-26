0ed7c370a19aac0079c22128c2b252e415e0447b
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-04-26 12:39:31 +0330

    fixed some issue

--- components/@web/Layout/Header/index.tsx
+++ components/@web/Layout/Header/index.tsx
@@ -1,6 +1,7 @@
 // "use client";
 
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { Session } from "@/types/interfaces";
 import { getServerSession } from "next-auth";
 import Logo from "./Logo";
 import Menu from "./Menu";
@@ -8,7 +9,7 @@ import Search from "./Search";
 import Tools from "./Tools";
 
 const Header = async () => {
-  const session = await getServerSession(authOptions);
+  const session: Session | null = await getServerSession(authOptions);
 
   return (
     <nav className="fixed w-full z-10 top-0 left-0 max-w-screen bg-white border-gray-200 shadow-lg h-16 ps-10 pe-0">
