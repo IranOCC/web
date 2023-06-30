16b13e9fa6b3225f61e40d0b35056755835e7a0f
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-07-01 02:43:49 +0330

    fixed login img

--- components/@auth/Layout/index.tsx
+++ components/@auth/Layout/index.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from "react";
 
+import CityBgRotated from "../../../assets/images/city-bg-rotated.png";
+
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="absolute h-full w-full flex ">
       <div className={`relative top-0 left-0 bg-gradient-to-r from-primary to-secondary w-4/5 lg:w-1/2 h-full overflow-hidden hidden md:block`}>
-        {/* <img src={LoginBackImage.src} /> */}
+        <img src={CityBgRotated.src} className="h-full rotate-12" />
         <div className="absolute top-0 left-0 w-full h-full" />
       </div>
       <div className="w-full h-full p-4">{children}</div>
