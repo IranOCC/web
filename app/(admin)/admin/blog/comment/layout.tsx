1f287c53960f1d4197412cf4358d2572e702cbe7
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-07-03 13:51:50 +0330

    fixed comment page

---
+++ app/(admin)/admin/blog/comment/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export const metadata = {
+  title: "دیدگاه ها و نظرات",
+};
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return children;
+};
+
+export default Layout;
