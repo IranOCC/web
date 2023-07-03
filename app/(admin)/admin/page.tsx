3f521f05b27769db7b9aa6b55e313235d75172e0
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-07-03 17:52:18 +0330

    fixed dashboard

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -10,54 +10,54 @@ export default function Page() {
     <>
       <div className="p-4">
         <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-12">
-          <div className="bg-white shadow-md rounded-lg p-4 order-1 lg:col-span-3">
+          <div className="bg-white shadow-md transition-shadow hover:shadow-lg rounded-lg p-4 order-1 lg:col-span-3">
             تعداد شعبات فعال
             {/*  */}
           </div>
-          <div className="bg-white shadow-md rounded-lg p-4 order-2 lg:col-span-3">
+          <div className="bg-white shadow-md transition-shadow hover:shadow-lg rounded-lg p-4 order-2 lg:col-span-3">
             تعداد کاربر فعال
             {/*  */}
           </div>
-          <div className="bg-white shadow-md rounded-lg p-4 order-3 lg:col-span-3">
+          <div className="bg-white shadow-md transition-shadow hover:shadow-lg rounded-lg p-4 order-3 lg:col-span-3">
             تعداد املاک ثبت شده
             {/*  */}
           </div>
-          <div className="bg-white shadow-md rounded-lg p-4 order-4 lg:col-span-3">
+          <div className="bg-white shadow-md transition-shadow hover:shadow-lg rounded-lg p-4 order-4 lg:col-span-3">
             تعداد پست های ثبت شده
             {/*  */}
           </div>
           {/* ==== */}
-          <div className="bg-white shadow-md rounded-lg p-4 order-5 sm:col-span-2 lg:col-span-6">
+          <div className="bg-white shadow-md transition-shadow hover:shadow-lg rounded-lg p-4 order-5 sm:col-span-2 lg:col-span-6">
             تعداد ملک در انتظار تایید
             {/*  */}
           </div>
-          <div className="bg-white shadow-md rounded-lg p-4 order-6 sm:col-span-2 lg:col-span-6">
+          <div className="bg-white shadow-md transition-shadow hover:shadow-lg rounded-lg p-4 order-6 sm:col-span-2 lg:col-span-6">
             تعداد ملک تایید شده
             {/*  */}
           </div>
           {/* ==== */}
-          <div className="bg-white shadow-md rounded-lg p-4 order-7 lg:col-span-3">
+          <div className="bg-white shadow-md transition-shadow hover:shadow-lg rounded-lg p-4 order-7 lg:col-span-3">
             ایجنت های برتر
             {/*  */}
           </div>
-          <div className="bg-white shadow-md rounded-lg p-4 order-9 lg:order-8 sm:col-span-2 lg:col-span-6">
+          <div className="bg-white shadow-md transition-shadow hover:shadow-lg rounded-lg p-4 order-9 lg:order-8 sm:col-span-2 lg:col-span-6">
             شعبات برتر
             {/*  */}
           </div>
-          <div className="bg-white shadow-md rounded-lg p-4 order-8 lg:order-9 lg:col-span-3">
+          <div className="bg-white shadow-md transition-shadow hover:shadow-lg rounded-lg p-4 order-8 lg:order-9 lg:col-span-3">
             نویسنده های برتر
             {/*  */}
           </div>
           {/* ==== */}
-          <div className="bg-white shadow-md rounded-lg p-4 order-10 sm:col-span-2 lg:col-span-4">
+          <div className="bg-white shadow-md transition-shadow hover:shadow-lg rounded-lg p-4 order-10 sm:col-span-2 lg:col-span-4">
             ملک های پر بازدید
             {/*  */}
           </div>
-          <div className="bg-white shadow-md rounded-lg p-4 order-11 sm:col-span-2 lg:col-span-4">
+          <div className="bg-white shadow-md transition-shadow hover:shadow-lg rounded-lg p-4 order-11 sm:col-span-2 lg:col-span-4">
             پست های پر بازدید
             {/*  */}
           </div>
-          <div className="bg-white shadow-md rounded-lg p-4 order-12 sm:col-span-2 lg:col-span-4">
+          <div className="bg-white shadow-md transition-shadow hover:shadow-lg rounded-lg p-4 order-12 sm:col-span-2 lg:col-span-4">
             آخرین نظرات
             {/*  */}
           </div>
