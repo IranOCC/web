49daa6748cb8ead688d4922c0bb12aaff133bf20
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2023-07-19 02:01:05 +0330

    Update layout.tsx

--- app/(web)/layout.tsx
+++ app/(web)/layout.tsx
@@ -18,7 +18,7 @@ export async function generateMetadata() {
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  return children;
+  // return children;
   return (
     <>
       <div
