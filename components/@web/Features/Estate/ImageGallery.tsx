339a86cbc93d15f3d6549c6cdb089f7142ed6431
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-07-26 15:39:32 +0330

    fixed gallery

--- components/@web/Features/Estate/ImageGallery.tsx
+++ components/@web/Features/Estate/ImageGallery.tsx
@@ -1,8 +1,10 @@
 import { useKeenSlider, KeenSliderPlugin, KeenSliderInstance } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
-import { MutableRefObject } from "react";
+import { MutableRefObject, useContext, useEffect, useState } from "react";
 import Image from "next/image";
 import { StorageFile } from "@/types/interfaces";
+import { Fullscreen, HomeMaxOutlined } from "@mui/icons-material";
+import { WebPreviewContext, WebPreviewContextType } from "@/context/webPreview.context";
 
 function ThumbnailPlugin(mainRef: MutableRefObject<KeenSliderInstance | null>): KeenSliderPlugin {
   return (slider) => {
@@ -37,65 +39,142 @@ function ThumbnailPlugin(mainRef: MutableRefObject<KeenSliderInstance | null>): 
   };
 }
 
+const AdaptiveHeight: KeenSliderPlugin = (slider) => {
+  function updateHeight() {
+    slider.container.style.height = slider.slides[slider.track.details.rel].offsetHeight + "px";
+  }
+  slider.on("created", () => setTimeout(updateHeight, 100));
+  slider.on("slideChanged", updateHeight);
+};
+
 const ImageGallery = ({ items }: { items: StorageFile[] }) => {
-  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
-    initial: 0,
-    mode: "free-snap",
-    rtl: true,
-  });
-  const [thumbnailRef] = useKeenSlider<HTMLDivElement>(
+  const { isFullscreen, isFullContent } = useContext(WebPreviewContext) as WebPreviewContextType;
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>(
     {
       initial: 0,
       mode: "free-snap",
-      rtl: false,
+      rtl: true,
+      slideChanged(s) {
+        setCurrentSlide(s.track.details.rel);
+      },
+      created() {
+        setLoaded(true);
+      },
+    },
+    [AdaptiveHeight]
+  );
+  const [thumbnailRef, thumbInstanceRef] = useKeenSlider<HTMLDivElement>(
+    {
+      initial: 0,
+      mode: "free-snap",
+      rtl: true,
       slides: {
         perView: "auto",
-        spacing: 5,
+        spacing: 16,
       },
     },
     [ThumbnailPlugin(instanceRef)]
   );
 
+  const adaptSize = () => {
+    const slider = instanceRef.current;
+    const thumb = thumbInstanceRef.current;
+    slider.container.style.width = slider.slides[slider.track.details.rel]?.parentNode?.parentNode?.offsetWidth + "px";
+    slider.container.style.height = slider.slides[slider.track.details.rel].offsetHeight + "px";
+    slider.update();
+    thumb.update();
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", adaptSize, false);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(adaptSize, 1000);
+  }, [isFullscreen, isFullContent]);
+
   return (
     <>
-      <div ref={sliderRef} className="keen-slider zoom-out">
-        {items.map(({ path, title, alt }) => {
-          return (
-            <div className="keen-slider__slide zoom-out__slide relative flex flex-col items-center justify-center overflow-hidden rounded-xl">
-              <Image
-                //
-                src={process.env.NEXT_PUBLIC_STORAGE_BASE_URL + "/" + path}
-                alt={alt}
-                title={title}
-                width={800}
-                height={400}
-                className="block rounded-xl object-contain"
-              />
-            </div>
-          );
-        })}
-      </div>
-      <div className="flex w-full flex-row py-4">
-        <div className="h-full bg-gray-200">
-          salam
-          {/*  */}
+      <div className="relative flex flex-col items-center justify-center overflow-hidden">
+        <div className="relative flex h-full w-full items-center overflow-hidden rounded-xl">
+          <div ref={sliderRef} className="keen-slider">
+            {items.map(({ path, title, alt }, idx) => {
+              return (
+                <div key={idx} className="keen-slider__slide relative flex !h-fit !min-h-fit flex-col items-center justify-center self-end overflow-hidden rounded-xl">
+                  <Image
+                    //
+                    src={process.env.NEXT_PUBLIC_STORAGE_BASE_URL + "/" + path}
+                    alt={alt}
+                    title={title}
+                    width={800}
+                    height={400}
+                    className="block rounded-xl object-contain"
+                  />
+                </div>
+              );
+            })}
+          </div>
+          {loaded && instanceRef.current && (
+            <>
+              <Arrow onClick={(e: any) => e.stopPropagation() || instanceRef.current?.prev()} disabled={currentSlide === 0} />
+              <Arrow left onClick={(e: any) => e.stopPropagation() || instanceRef.current?.next()} disabled={currentSlide === instanceRef.current.track.details.slides.length - 1} />
+            </>
+          )}
         </div>
-        <div ref={thumbnailRef} className="keen-slider">
-          {items.map(({ path, title, alt }) => {
-            return (
-              <div className="keen-slider__slide group relative flex min-w-[5rem] max-w-[5rem] cursor-pointer flex-col items-center justify-center overflow-hidden rounded-xl">
-                <Image
-                  //
-                  src={process.env.NEXT_PUBLIC_STORAGE_BASE_URL + "/" + path}
-                  alt={alt}
-                  title={title}
-                  width={180}
-                  height={100}
-                  className="block h-full rounded-xl border-2 border-transparent object-fill group-[.active]:border-secondary"
-                />
+        <div className="flex w-full flex-row gap-4 py-4">
+          <div className="hidden h-full md:block">
+            <div className="relative flex h-28 flex-row items-center justify-center gap-1.5 rounded-2xl bg-gray-200 px-1 text-gray-700">
+              {/*  */}
+              <div className="flex w-20 min-w-[5rem] max-w-[5rem] flex-col items-center justify-center gap-1.5">
+                <span>متراژ بنا</span>
+                <Fullscreen />
+                <b className="text-center text-base font-extrabold leading-none text-black">250 مترمربع</b>
               </div>
-            );
-          })}
+              {/*  */}
+              <div className=" my-3 h-full border border-gray-300" />
+              {/*  */}
+              <div className="flex w-20 min-w-[5rem] max-w-[5rem] flex-col items-center justify-center gap-1.5">
+                <span>متراژ بنا</span>
+                <Fullscreen />
+                <b className="text-center text-base font-extrabold leading-none text-black">250 مترمربع</b>
+              </div>
+              {/*  */}
+              <div className=" my-3 h-full border border-gray-300" />
+              {/*  */}
+              <div className="flex w-20 min-w-[5rem] max-w-[5rem] flex-col items-center justify-center gap-1.5">
+                <span>متراژ بنا</span>
+                <Fullscreen />
+                <b className="text-center text-base font-extrabold leading-none text-black">250 مترمربع</b>
+              </div>
+              {/*  */}
+              <div className=" my-3 h-full border border-gray-300" />
+              {/*  */}
+              <div className="flex w-20 min-w-[5rem] max-w-[5rem] flex-col items-center justify-center gap-1.5">
+                <span>متراژ بنا</span>
+                <Fullscreen />
+                <b className="text-center text-base font-extrabold leading-none text-black">250 مترمربع</b>
+              </div>
+            </div>
+          </div>
+          <div ref={thumbnailRef} className="keen-slider">
+            {items.map(({ path, title, alt }, idx) => {
+              return (
+                <div key={idx} className="keen-slider__slide group relative flex aspect-square h-28 w-28 min-w-[7rem] max-w-[7rem] cursor-pointer flex-col items-center justify-center overflow-hidden rounded-xl">
+                  <Image
+                    //
+                    src={process.env.NEXT_PUBLIC_STORAGE_BASE_URL + "/" + path}
+                    alt={alt}
+                    title={title}
+                    width={112}
+                    height={112}
+                    className="block h-full rounded-xl border-2 border-transparent object-fill group-[.active]:border-secondary"
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </>
@@ -103,3 +182,16 @@ const ImageGallery = ({ items }: { items: StorageFile[] }) => {
 };
 
 export default ImageGallery;
+
+function Arrow(props: { disabled: boolean; left?: boolean; onClick: (e: any) => void }) {
+  return (
+    <div onClick={props.onClick} className={`${props.left ? "left-0" : "left-auto right-0"} ${props.disabled ? "cursor-not-allowed" : "cursor-pointer"} absolute flex h-full items-center justify-center`}>
+      <div className="rounded-xl bg-gray-100 p-2">
+        <svg className={`${props.disabled ? "fill-gray-200" : "fill-gray-400/80"} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          {props.left && <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />}
+          {!props.left && <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />}
+        </svg>
+      </div>
+    </div>
+  );
+}
--- components/@web/Features/Estate/SingleEstate.tsx
+++ components/@web/Features/Estate/SingleEstate.tsx
@@ -7,10 +7,10 @@ import { useContext, useEffect } from "react";
 import ImageGallery from "./ImageGallery";
 
 const SingleEstate = ({ data }: { data: WebEstate }) => {
-  const { _id, title, slug, content, excerpt, tags, category, image, gallery, pinned, publishedAt, owner, code } = data;
+  const { _id, title, slug, content, excerpt, tags, category, image, gallery, pinned, publishedAt, owner, code, province, city, district } = data;
   const { singleEstate } = useContext(WebPreviewContext) as WebPreviewContextType;
   useEffect(() => {
-    singleEstate(_id, title, category, code || "-", "");
+    singleEstate(_id, title, category, code || "-", [province, city, district].join(" - "));
   }, []);
   useEffect(() => {
     //
@@ -18,14 +18,9 @@ const SingleEstate = ({ data }: { data: WebEstate }) => {
 
   return (
     <>
-      {!!gallery?.length && (
-        <div className="relative flex flex-col items-center justify-center overflow-hidden">
-          {/*  */}
-          <ImageGallery items={gallery} />
-          {/*  */}
-        </div>
-      )}
+      {!!gallery?.length && <ImageGallery items={gallery} />}
       {/* <div className="relative mt-4" dangerouslySetInnerHTML={{ __html: content }} /> */}
+      سلام خوبی
     </>
   );
 };
