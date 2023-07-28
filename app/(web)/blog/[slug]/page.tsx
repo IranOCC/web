e85b69c708c1166b4fedd39cee3c4775ff9c5fd0
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-07-28 17:35:04 +0330

    fixed blog metadata

--- app/(web)/blog/[slug]/page.tsx
+++ app/(web)/blog/[slug]/page.tsx
@@ -1,5 +1,19 @@
 import SingleBlogPost from "@/components/@web/Features/Blog/SingleBlogPost";
 import { fetchSingleBlogPost } from "@/lib/ssr.fetch";
+import { Metadata } from "next";
+
+export async function generateMetadata({ params }: any): Promise<Metadata> {
+  const data = await fetchSingleBlogPost(params.slug);
+  const { title, excerpt, tags, image } = data;
+  return {
+    title,
+    description: excerpt,
+    keywords: tags,
+    openGraph: {
+      images: image && { url: process.env.NEXT_PUBLIC_STORAGE_BASE_URL + "/" + image.path },
+    },
+  };
+}
 
 export default async function Page({ params }: any) {
   const data = await fetchSingleBlogPost(params.slug);
