d6bd6dd1cb4f16dabebe093a7b2c71c40a774eae
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-09-18 17:19:10 +0330

    remove cards

--- components/@panel/Dashboard/ContentStatistics.tsx
+++ components/@panel/Dashboard/ContentStatistics.tsx
@@ -1,5 +1,5 @@
 import { Button, Card, CardFooter, Image, CardHeader, Tabs, Tab } from "@nextui-org/react";
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, LineChart, Tooltip, ResponsiveContainer, Line, Legend } from "recharts";
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, LineChart, Tooltip, ResponsiveContainer, Line, Legend, Brush } from "recharts";
 import { Spinner } from "@nextui-org/react";
 import { useState, Key, useEffect } from "react";
 import { Listbox, ListboxItem } from "@nextui-org/react";
@@ -117,40 +117,6 @@ export const EstatesStatistics = () => {
     <Card className={"group w-auto bg-white/80" + (loading ? " is-loading" : "")}>
       <CardHeader className="relative z-10 flex flex-col items-start gap-2">
         <h4 className="truncate text-base font-bold">آمار ملک ها</h4>
-        {/* <Listbox
-          aria-label="Detail"
-          className="absolute end-3 max-w-[96px] gap-0 divide-y divide-default-300/50 overflow-hidden rounded-medium bg-black/70 p-0 text-white opacity-50 shadow-small transition-all hover:max-w-[180px] hover:opacity-100 dark:divide-default-100/80"
-          itemClasses={{
-            base: "px-3 first:rounded-t-medium last:rounded-b-medium rounded-none gap-3 h-12 data-[hover=true]:bg-default-100/80",
-          }}
-        >
-          <ListboxItem
-            key="total"
-            endContent={<b className="text-lg">25</b>}
-            startContent={
-              <IconWrapper className="bg-[#0088FE]/10 text-[#0088FE]">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-5 w-5" fill="currentColor">
-                  <path d="M21 19H23V21H1V19H3V4C3 3.44772 3.44772 3 4 3H14C14.5523 3 15 3.44772 15 4V19H17V9H20C20.5523 9 21 9.44772 21 10V19ZM7 11V13H11V11H7ZM7 7V9H11V7H7Z"></path>
-                </svg>
-              </IconWrapper>
-            }
-          >
-            همه
-          </ListboxItem>
-          <ListboxItem
-            key="confirmed"
-            endContent={<b className="text-lg">10</b>}
-            startContent={
-              <IconWrapper className="bg-[#00C49F]/10 text-[#00C49F]">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-5 w-5" fill="currentColor">
-                  <path d="M21 19H23V21H1V19H3V4C3 3.44772 3.44772 3 4 3H14C14.5523 3 15 3.44772 15 4V19H19V11H17V9H20C20.5523 9 21 9.44772 21 10V19ZM5 5V19H13V5H5ZM7 11H11V13H7V11ZM7 7H11V9H7V7Z"></path>
-                </svg>
-              </IconWrapper>
-            }
-          >
-            تایید شده
-          </ListboxItem>
-        </Listbox> */}
       </CardHeader>
       <LineChartMode
         data={data}
@@ -205,6 +171,7 @@ const LineChartMode = ({ data, items }: { data: any[]; items: any[] }) => {
           {items.map(({ key, name, fill }) => (
             <Line type="monotone" key={key} dataKey={key} name={name} stroke={fill} activeDot={{ r: 6 }} />
           ))}
+          <Brush dataKey="name" height={30} stroke="#8884d8" />
         </LineChart>
       </ResponsiveContainer>
     </div>
