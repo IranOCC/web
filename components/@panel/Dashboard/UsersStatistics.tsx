4d04b82e9b27b85a51a3907845e9d5da9b86a42a
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-09-19 09:29:24 +0330

    fixed some issue

--- components/@panel/Dashboard/UsersStatistics.tsx
+++ components/@panel/Dashboard/UsersStatistics.tsx
@@ -326,10 +326,31 @@ const LineChartMode = ({ data }: { data: { data: any[]; items: any[] } }) => {
           <Tooltip wrapperClassName="text-right text-sm" />
           <Legend />
           {data.items.map(({ _id, name }) => (
-            <Line type="monotone" key={_id} dataKey={_id} name={name} activeDot={{ r: 6 }} />
+            <Line
+              //
+              type="monotone"
+              key={_id}
+              dataKey={_id}
+              name={name}
+              stroke={stringToColor(_id)}
+              activeDot={{ r: 6 }}
+            />
           ))}
         </LineChart>
       </ResponsiveContainer>
     </div>
   );
+};
+
+const stringToColor = (str: string) => {
+  let hash = 0;
+  str.split("").forEach((char) => {
+    hash = char.charCodeAt(0) + ((hash << 5) - hash);
+  });
+  let color = "#";
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += value.toString(16).padStart(2, "0");
+  }
+  return color;
 };
