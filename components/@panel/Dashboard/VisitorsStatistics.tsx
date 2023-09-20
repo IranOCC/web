a110a1b01742d38720bef7843008559aef45b3a9
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-09-20 10:00:17 +0330

    fixed time filtering

--- components/@panel/Dashboard/VisitorsStatistics.tsx
+++ components/@panel/Dashboard/VisitorsStatistics.tsx
@@ -1,22 +1,50 @@
-import { Button, Card, CardFooter, Image, CardHeader, Tabs, Tab, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
+import { Button, Card, CardFooter, Image, CardHeader, Tabs, Tab, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Chip } from "@nextui-org/react";
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, LineChart, Tooltip, ResponsiveContainer, Line, Brush, BarChart, Legend, Bar, Sector, PieChart, Pie, Cell } from "recharts";
-import { Select, SelectSection, SelectItem } from "@nextui-org/react";
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@nextui-org/react";
 import { Listbox, ListboxItem } from "@nextui-org/react";
 import { cn } from "@nextui-org/react";
 import { Spinner } from "@nextui-org/react";
-import { useState, Key, useEffect } from "react";
+import { useState, Key, useEffect, useMemo } from "react";
 import useAxiosAuth from "@/hooks/useAxiosAuth";
+import moment from "jalali-moment";
 
 export const VisitorsStatistics = () => {
   const [loading, setLoading] = useState(false);
   const [report, setReport] = useState<Key>("visitor");
+  const [rangeDateType, setRangeDateType] = useState<any>(new Set(["yesterday"]));
   const [data, setData] = useState([]);
+
+  const rangeValue = useMemo(() => {
+    return Array.from(rangeDateType).join(", ");
+  }, [rangeDateType]);
+
+  const rangeValueTL = useMemo(() => {
+    switch (rangeValue) {
+      case "yesterday":
+        return "دیروز";
+      case "today":
+        return "امروز";
+      case "7daysAgo":
+        return "7 روز اخیر";
+      case "thisWeek":
+        return "این هفته";
+      case "lastWeek":
+        return "هفته قبل";
+      case "30daysAgo":
+        return "30 روز اخیر";
+      case "thisMonth":
+        return "این ماه";
+      case "lastMonth":
+        return "ماه گذشته";
+    }
+    return null;
+  }, [rangeValue]);
 
   const api = useAxiosAuth();
   const getData = async () => {
     setLoading(true);
     try {
-      const response = await api.get(`/admin/dashboard/visitors/${report}`);
+      const response = await api.get(`/admin/dashboard/visitors/${report}?range=${rangeValue}`);
       setData(response.data);
       setLoading(false);
     } catch (error) {
@@ -25,62 +53,116 @@ export const VisitorsStatistics = () => {
   };
   useEffect(() => {
     getData();
-  }, [report]);
+  }, [report, rangeValue]);
 
   return (
     <Card className={"group w-auto bg-white/80" + (loading ? " is-loading" : "")}>
       <CardHeader className="relative z-10 flex flex-col items-start gap-2">
-        {/*
         <Listbox
-          aria-label="Detail"
-          className="absolute end-3 max-w-[96px] gap-0 divide-y divide-default-300/50 overflow-hidden rounded-medium bg-black/70 p-0 text-white opacity-50 shadow-small transition-all hover:max-w-[260px] hover:opacity-100 dark:divide-default-100/80"
+          aria-label="filtering"
+          className="absolute start-3 max-w-[44px] gap-0 divide-y divide-default-300/50 overflow-hidden rounded-medium bg-black/70 p-0 text-white opacity-50 shadow-small transition-all hover:max-w-[260px] hover:opacity-100 dark:divide-default-100/80"
           itemClasses={{
-            base: "px-3 first:rounded-t-medium last:rounded-b-medium rounded-none gap-3 h-12 data-[hover=true]:bg-default-100/80",
+            base: "px-3 first:rounded-t-medium last:rounded-b-medium rounded-none gap-3 h-12 data-[hover=true]:bg-transparent data-[hover=true]:text-white",
           }}
         >
           <ListboxItem
-            key="online-users"
-            endContent={<b className="text-lg">25</b>}
-            startContent={
-              <IconWrapper className="bg-success/10 text-success">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-5 w-5" fill="currentColor">
-                  <path d="M13 14.0619V22H4C4 17.5817 7.58172 14 12 14C12.3387 14 12.6724 14.021 13 14.0619ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13ZM17.7929 19.9142L21.3284 16.3787L22.7426 17.7929L17.7929 22.7426L14.2574 19.2071L15.6716 17.7929L17.7929 19.9142Z"></path>
-                </svg>
-              </IconWrapper>
+            key="choose-date-range"
+            endContent={
+              <div className="flex items-center gap-1">
+                <Chip color="secondary" variant="solid">
+                  {rangeValueTL}
+                </Chip>
+                <Dropdown>
+                  <DropdownTrigger>
+                    <Button isIconOnly radius="full" size="sm" variant="flat">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-5 w-5" fill="currentColor">
+                        <path d="M21 18V21H19V18H17V16H23V18H21ZM5 18V21H3V18H1V16H7V18H5ZM11 6V3H13V6H15V8H9V6H11ZM11 10H13V21H11V10ZM3 14V3H5V14H3ZM19 14V3H21V14H19Z"></path>
+                      </svg>
+                    </Button>
+                  </DropdownTrigger>
+                  <DropdownMenu
+                    //
+                    aria-label="Choose date range"
+                    disallowEmptySelection
+                    selectionMode="single"
+                    selectedKeys={rangeDateType}
+                    onSelectionChange={setRangeDateType}
+                  >
+                    <DropdownItem
+                      //
+                      key="yesterday"
+                      description={moment().subtract(1, "jDay").locale("fa").format("DD MMM YYYY")}
+                    >
+                      دیروز
+                    </DropdownItem>
+                    <DropdownItem
+                      //
+                      showDivider
+                      key="today"
+                      description={moment().locale("fa").format("DD MMM YYYY")}
+                    >
+                      امروز
+                    </DropdownItem>
+                    <DropdownItem
+                      //
+                      key="7daysAgo"
+                      description={`${moment().subtract(8, "jDay").locale("fa").format("DD MMM YYYY")} - ${moment().subtract(1, "jDay").locale("fa").format("DD MMM YYYY")}`}
+                    >
+                      7 روز اخیر
+                    </DropdownItem>
+                    <DropdownItem
+                      //
+                      key="thisWeek"
+                      description={`${moment().startOf("week").locale("fa").format("DD MMM YYYY")} - ${moment().endOf("week").locale("fa").format("DD MMM YYYY")}`}
+                    >
+                      این هفته
+                    </DropdownItem>
+                    <DropdownItem
+                      //
+                      showDivider
+                      key="lastWeek"
+                      description={`${moment().subtract(1, "week").startOf("week").locale("fa").format("DD MMM YYYY")} - ${moment().subtract(1, "week").endOf("week").locale("fa").format("DD MMM YYYY")}`}
+                    >
+                      هفته قبل
+                    </DropdownItem>
+                    <DropdownItem
+                      //
+                      key="30daysAgo"
+                      description={`${moment().subtract(31, "jDay").locale("fa").format("DD MMM YYYY")} - ${moment().subtract(1, "jDay").locale("fa").format("DD MMM YYYY")}`}
+                    >
+                      30 روز اخیر
+                    </DropdownItem>
+                    <DropdownItem
+                      //
+                      key="thisMonth"
+                      description={`${moment().startOf("jMonth").locale("fa").format("MMM YYYY")}`}
+                    >
+                      این ماه
+                    </DropdownItem>
+                    <DropdownItem
+                      //
+                      key="lastMonth"
+                      description={`${moment().subtract(1, "jMonth").startOf("jMonth").locale("fa").format("MMM YYYY")}`}
+                    >
+                      ماه گذشته
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </div>
             }
-          >
-            کاربران آنلاین
-          </ListboxItem>
-          <ListboxItem
-            key="today-google"
-            endContent={<b className="text-lg">10</b>}
             startContent={
               <IconWrapper className="bg-danger/10 text-danger">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-5 w-5" fill="currentColor">
-                  <path d="M3.06364 7.50914C4.70909 4.24092 8.09084 2 12 2C14.6954 2 16.959 2.99095 18.6909 4.60455L15.8227 7.47274C14.7864 6.48185 13.4681 5.97727 12 5.97727C9.39542 5.97727 7.19084 7.73637 6.40455 10.1C6.2045 10.7 6.09086 11.3409 6.09086 12C6.09086 12.6591 6.2045 13.3 6.40455 13.9C7.19084 16.2636 9.39542 18.0227 12 18.0227C13.3454 18.0227 14.4909 17.6682 15.3864 17.0682C16.4454 16.3591 17.15 15.3 17.3818 14.05H12V10.1818H21.4181C21.5364 10.8363 21.6 11.5182 21.6 12.2273C21.6 15.2727 20.5091 17.8363 18.6181 19.5773C16.9636 21.1046 14.7 22 12 22C8.09084 22 4.70909 19.7591 3.06364 16.4909C2.38638 15.1409 2 13.6136 2 12C2 10.3864 2.38638 8.85911 3.06364 7.50914Z"></path>
+                  <path d="M17 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H7V1H9V3H15V1H17V3ZM4 9V19H20V9H4ZM6 11H8V13H6V11ZM11 11H13V13H11V11ZM16 11H18V13H16V11Z"></path>
                 </svg>
               </IconWrapper>
             }
           >
-            گوگل امروز
-          </ListboxItem>
-          <ListboxItem
-            key="today-users"
-            endContent={<b className="text-lg">17</b>}
-            startContent={
-              <IconWrapper className="bg-warning/10 text-warning">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-5 w-5" fill="currentColor">
-                  <path d="M2 22C2 17.5817 5.58172 14 10 14C14.4183 14 18 17.5817 18 22H2ZM10 13C6.685 13 4 10.315 4 7C4 3.685 6.685 1 10 1C13.315 1 16 3.685 16 7C16 10.315 13.315 13 10 13ZM17.3628 15.2332C20.4482 16.0217 22.7679 18.7235 22.9836 22H20C20 19.3902 19.0002 17.0139 17.3628 15.2332ZM15.3401 12.9569C16.9728 11.4922 18 9.36607 18 7C18 5.58266 17.6314 4.25141 16.9849 3.09687C19.2753 3.55397 21 5.57465 21 8C21 10.7625 18.7625 13 16 13C15.7763 13 15.556 12.9853 15.3401 12.9569Z"></path>
-                </svg>
-              </IconWrapper>
-            }
-          >
-            بازدیدکنندگان امروز
+            بازه زمانی
           </ListboxItem>
         </Listbox>
-        */}
       </CardHeader>
-      {report === "visitor" && <LineChartMode data={data} items={[{ name: "کاربران", key: "count", fill: "rgb(245, 165, 36)" }]} />}
+      {report === "visitor" && <LineChartMode data={data} items={[{ name: "کاربران", key: "count", fill: "rgb(243, 18, 96)" }]} />}
       {report !== "visitor" && <PieChartMode data={data} />}
 
       <CardFooter className="border-zinc-100/50 z-10 gap-2 border-t-1 bg-black/70">
