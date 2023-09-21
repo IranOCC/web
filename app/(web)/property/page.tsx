0cb6c9fece790b88b16cf1977bc35ea2b435e29e
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-09-21 15:48:00 +0330

    remove fetch

--- app/(web)/property/page.tsx
+++ app/(web)/property/page.tsx
@@ -6,11 +6,16 @@ type Props = {
 };
 
 export default async function Page(params: Props) {
-  const data = await fetchEstateList(new URLSearchParams(params.searchParams));
+  // const data = await fetchEstateList(new URLSearchParams(params.searchParams));
   return (
     <>
       <div className="flex h-auto min-h-full flex-col bg-gray-200 px-3 pb-20 md:bg-transparent md:px-4 md:pb-4">
-        <EstateList data={data} />
+        <EstateList
+          data={{
+            items: [],
+            total: 0,
+          }}
+        />
       </div>
     </>
   );
