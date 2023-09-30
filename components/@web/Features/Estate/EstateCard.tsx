a1c6bda94346fe991a5b4acd45df0537bb83de0b
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-09-30 12:33:15 +0330

    fixed rent in list

--- components/@web/Features/Estate/EstateCard.tsx
+++ components/@web/Features/Estate/EstateCard.tsx
@@ -4,7 +4,7 @@ import { WebEstate } from "@/types/interfaces";
 import Image from "next/image";
 import Link from "next/link";
 import FeaturesList from "./FeaturesList";
-import { Button, Card } from "@nextui-org/react";
+import { Button, Card, Chip } from "@nextui-org/react";
 import { ReactNode, useRef, useState } from "react";
 import FavoriteButton from "../@common/FavoriteButton";
 import { Call, FavoriteBorderOutlined, Visibility } from "@mui/icons-material";
@@ -45,6 +45,10 @@ const EstateCard = ({ data, tools, toolsClassName = "" }: { data: WebEstate; too
     unitsCount,
     floor,
     withOldBuilding,
+
+    dailyRent,
+    annualRent,
+    special,
   } = data;
 
   const [startPoint, setStartPoint] = useState(0);
@@ -107,8 +111,34 @@ const EstateCard = ({ data, tools, toolsClassName = "" }: { data: WebEstate; too
                     height={200}
                     className="block max-h-[10rem] w-full rounded-xl object-cover md:h-full md:max-h-[12rem] md:min-h-[12rem] md:max-w-[12rem] lg:max-w-[15rem] xl:max-w-[20rem]"
                   />
+                  {(dailyRent || annualRent) && (
+                    <div className="absolute right-3 top-3 block">
+                      <div className="relative flex flex-col gap-1">
+                        {dailyRent && (
+                          <Chip variant="shadow" color="secondary" className="text-sm">
+                            اجاره روزانه
+                          </Chip>
+                        )}
+                        {annualRent && (
+                          <Chip variant="shadow" color="secondary" className="text-sm">
+                            اجاره سالانه
+                          </Chip>
+                        )}
+                      </div>
+                    </div>
+                  )}
                   <div className="flex w-full flex-col justify-center gap-2 md:py-3">
-                    <h3 className="w-full truncate text-start font-bold">{title}</h3>
+                    <h3 className="flex w-full flex-row gap-2 text-start font-bold">
+                      {special && (
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-8 w-8 text-secondary" fill="currentColor">
+                          <path
+                            fill="currentColor"
+                            d="M3.49709 8.06489L4.78355 18.9998H19.2266L20.513 8.06489L16.5032 10.7381L12.0051 4.44076L7.50694 10.7381L3.49709 8.06489ZM2.80577 5.20031L7.00505 7.99983L11.1913 2.13905C11.5123 1.68964 12.1369 1.58555 12.5863 1.90656C12.6761 1.9707 12.7546 2.04926 12.8188 2.13905L17.0051 7.99983L21.2043 5.20031C21.6639 4.89395 22.2847 5.01813 22.5911 5.47766C22.7228 5.67527 22.7799 5.91332 22.7522 6.1492L21.109 20.1167C21.0497 20.6203 20.6229 20.9998 20.1158 20.9998H3.8943C3.38722 20.9998 2.9604 20.6203 2.90115 20.1167L1.25792 6.1492C1.19339 5.6007 1.58573 5.10374 2.13423 5.03921C2.37011 5.01146 2.60816 5.06856 2.80577 5.20031ZM12.0051 14.9998C10.9005 14.9998 10.0051 14.1044 10.0051 12.9998C10.0051 11.8953 10.9005 10.9998 12.0051 10.9998C13.1096 10.9998 14.0051 11.8953 14.0051 12.9998C14.0051 14.1044 13.1096 14.9998 12.0051 14.9998Z"
+                          />
+                        </svg>
+                      )}
+                      {title}
+                    </h3>
                     <FeaturesList data={data} isEstateCard />
                   </div>
                 </div>
